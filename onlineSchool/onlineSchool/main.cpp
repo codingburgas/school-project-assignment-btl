37e7a8fdd90ea96eec82d8b1f87fbe99d9e96680
KDIliev22 <kdiliev22@codingburgas.bg>
2024-04-21 17:00:39 +0300

    Reworked main.cpp

--- onlineSchool/onlineSchool/main.cpp
+++ onlineSchool/onlineSchool/main.cpp
@@ -1,30 +1,38 @@
 #include "raylib.h"
 #include "login.h"
 #include "home.h"
+#include "exam.h"
 
 int main() {
-
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "Login Page");
 
     Login loginPage;
+    Exam exam;
     Home homeScreen(loginPage);
 
     while (!WindowShouldClose()) {
-        if (!loginPage.IsLoggedIn()) {
+        if (!loginPage.IsLoggedIn() && homeScreen.isTesting == false) {
             loginPage.Update();
             BeginDrawing();
             ClearBackground(DARKBLUE);
             loginPage.Draw();
             EndDrawing();
         }
-        else {
+        if (loginPage.IsLoggedIn() && homeScreen.isTesting == false) {
             homeScreen.Update();
             BeginDrawing();
             ClearBackground(DARKBLUE);
             homeScreen.Draw();
+            EndDrawing();
+        }
+        if (homeScreen.isTesting == true) {
+            exam.Update();
+            BeginDrawing();
+            ClearBackground(DARKBLUE);
+            exam.Draw();
             EndDrawing();
         }
     }
